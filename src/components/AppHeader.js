26ed569dae97bb2931de336afd1f128ecfba7e92
davidauza.engineer <davidauza01@gmail.com>
2021-04-03 17:17:20 -0500

    Update src/components/AppHeader.js

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import axios from 'axios';
 
+const handleSignOut = function(event) {
+  event.preventDefault();
+  axios({
+    method: 'DELETE',
+    url: 'http://localhost:3001/auth/sign_out',
+    data: JSON.parse(localStorage.user)
+  })
+  .then(()=> {
+    localStorage.removeItem('user');
+    window.location = '/';
+  });
+}
+
 function AppHeader(props) {
+  const currentUser = localStorage.getItem('user');
   return(
     <div>
-      <div style={ { float: 'right' } }>
-        <a href="#">Sign out</a>
-      </div>
+      {currentUser &&
+        <div style={ { float: 'right' } }>
+          {JSON.parse(currentUser).uid}
+          <a href="#" onClick={handleSignOut}>Sign out</a>
+        </div>
+      }
       <h1 className="logo" style={ { color: props.logoColor } }>Eventlite</h1>
     </div>  
   )
